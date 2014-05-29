c95f9d8d8c29905c5f542630ee019380b83f309a
mgalindo <mgalindob@gmail.com>
2014-05-29 17:45:37 -0500

    Interim Checkin

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(901, 74);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(739, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Show Dev Tools";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(739, 11);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Refresh";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1003, 544);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -52,5 +52,71 @@ namespace Test
                     break;
             }
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            JsSetElemeValuetById("edID", "xxx");
+            JsclickElementById("btnApply");
+
+            JsSetElemeValuetById("edLatitude", "33.43017060777363"); //edLatitude.Text);
+            JsclickElementById("btnApply");
+
+            JsSetElemeValuetById("edLongitude", "-86.69652080535889"); //edLongitude.Text);
+            JsclickElementById("btnApply");
+
+            JsclickElementById("btnAddUpdateMarker");
+        }
+
+        private void JsclickElementById(string elementID)
+        {
+            JsFireEvent("document.getElementById('" + elementID + "')", "click");
+
+        }
+
+        public void JsFireEvent(string getElementQuery, string eventName)
+        {
+            webView.ExecuteScript(@"
+                                function fireEvent(element,event) {
+                                    var evt = document.createEvent('HTMLEvents');
+                                    evt.initEvent(event, true, true ); // event type,bubbling,cancelable
+                                    element.dispatchEvent(evt);                                 
+                                }
+                                " + String.Format("fireEvent({0}, '{1}');", getElementQuery, eventName));
+        }
+
+        private void JsSetElemeValuetById(string elementID, string elementValue)
+        {
+
+            webView.ExecuteScript(String.Format("document.getElementById('{0}').value='{1}'", elementID, elementValue));
+
+//            dynamic textboxes = (JSObject)webView.ExecuteJavascriptWithResult("document.getElementsByTagName('input')");
+//            int len = textboxes.length;
+//            for (int i = 0; i < len; i++)
+//            {
+//                textboxes[i].value = elementValue;
+//            }
+
+        }
+
+        private void btnClearMarkers_Click(object sender, EventArgs e)
+        {
+            clearAllMarkers();
+        }
+
+        private void clearAllMarkers()
+        {
+            JsclickElementById("btmRemoveAllMarkers");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webView.ShowDevTools();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            webView.Reload();
+        }
+
     }
 }
