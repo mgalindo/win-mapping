6f191c5855056eacb29599f3f1d20afd9b91fe3b
mgalindo <mgalindob@gmail.com>
2014-05-29 10:57:54 -0500

    Initial Commit

---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using CefSharp.WinForms;
+using CefSharp;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        WebView webView;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webView = new WebView("http://mgevodev64-vm:5000", new BrowserSettings());
+            webView.PropertyChanged += new PropertyChangedEventHandler(webView_PropertyChanged);
+            webView.Dock = DockStyle.Fill;
+            this.Controls.Add(webView);
+        }
+        
+        // Get all propertys from https://github.com/chillitom/CefSharp/blob/master/CefSharp/BrowserCore.h
+        void webView_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            BrowserCore core = (BrowserCore)sender;
+            switch (e.PropertyName)
+            {
+                case "IsBrowserInitialized":
+                    //core.IsBrowserInitialized
+                    break;
+                case "Title":
+                    //core.Title
+                    break;
+                case "Address":
+                    //core.Address
+                    break;
+                case "CanGoBack":
+                    //core.CanGoBack
+                    break;
+                case "CanGoForward":
+                    //core.CanGoForward;
+                    break;
+                case "IsLoading":
+                    //core.IsLoading
+                    break;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-win-mapping
-===========
+# CefSharp - Embedded Chromium for .Net
+
+This is an example showing the use of CefSharp Winforms in C#.
+
+Use this as Template for your Quick'n'Dirty Webbrowser Projects.
+
+For more info see:
+https://github.com/chillitom/CefSharp
